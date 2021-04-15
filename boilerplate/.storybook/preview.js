e4f547a457bba0e05a12437c9ed99d03ba8aa28b
Bruno Matheus <bmatheusc@gmail.com>
2021-04-15 00:45:12 -0300

    Adding new types for storybook

--- boilerplate/.storybook/preview.js
+++ boilerplate/.storybook/preview.js
@@ -1,14 +1,20 @@
-import { addDecorator } from '@storybook/react';
-import withGlobalStyles from './withGlobalStyles';
-
-addDecorator(withGlobalStyles);
+import Global from '../src/styles/global';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
 }
+
+export const decorators = [
+	(Story) => (
+		<>
+			<Global />
+			<Story />
+		</>
+	)
+];
